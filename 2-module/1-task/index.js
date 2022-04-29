f91cb0ee8b06cbee83d1b75ebcbce116b208c92f
Gtronicwave <101455651+Gtronicwave@users.noreply.github.com>
2022-04-29 11:27:22 +0200

    Done tasks from mudule 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sumSalary(salaries) {
-  // ваш код...
+	let sumOfSalaries = 0;
+
+	for (let key in salaries) {
+		let isSalary = typeof salaries[key] === 'number' && Number.isFinite(salaries[key]);
+
+		if (isSalary) {
+			sumOfSalaries += salaries[key];
+		}
+	}
+
+	return sumOfSalaries;
 }
+
+
+
